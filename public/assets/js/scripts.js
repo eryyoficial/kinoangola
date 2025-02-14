a1aec783c693f6fca1511bf3cd3ff4b968fcbaa3
Hermínio Da Silva André Francisco <154982502+eryyoficial@users.noreply.github.com>
2025-02-15 00:37:38 +0100

    section PESQUISA FILTRADA DONE

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -17,16 +17,37 @@ var swiper = new Swiper(".HeroSwiper", {
 
 
 var swiper = new Swiper(".PesquisaSwiper", {
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
+        dynamicBullets: true,
         clickable: true,
         renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + "</span>";
+            return '<span class="' + className + '"><p>' + (index + 1) + "</p></span>";
         },
-      },
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    loop:true,
 });
+
+// Pára o autoplay quando o mouse passar sobre um swiper-slide
+document.querySelectorAll('.PesquisaSwiper .swiper-slide').forEach(function(slide) {
+    slide.addEventListener('mouseenter', function () {
+        swiper.autoplay.stop();
+    });
+
+    // Retoma o autoplay quando o mouse sair do swiper-slide
+    slide.addEventListener('mouseleave', function () {
+        swiper.autoplay.start();
+    });
+});
+
