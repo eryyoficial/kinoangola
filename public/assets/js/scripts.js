ce8e95849db688d8eec73e08412ba852963109ec
Hermínio Da Silva André Francisco <154982502+eryyoficial@users.noreply.github.com>
2025-04-06 15:17:08 +0100

    first

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -173,3 +173,81 @@ const mobileNavbar = new MobileNavbar(
 mobileNavbar.init(); 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Função para ocultar o texto, aumentar o tamanho dos ícones e manter visíveis os ícones
+function adjustDisplay() {
+  const spans = document.querySelectorAll('.tagincolhe');
+
+   // Verifica se a largura da tela é menor ou igual a 600px
+   if (window.innerWidth <= 360) {
+    spans.forEach(span => {
+      // Aumenta o tamanho do ícone para 2rem
+      const icon = span.querySelector('i');
+      if (icon) {
+        icon.style.fontSize = '.9rem';
+      }
+
+      // Aumenta o padding da tag <span> para 1rem
+      span.style.padding = '0rem';
+      span.style.padding = '.4rem';
+
+      // Esconde o texto e deixa visível o ícone
+      span.childNodes.forEach(child => {
+        if (child.nodeType === Node.TEXT_NODE) {
+          child.textContent = ''; // Esconde o texto
+        }
+      });
+    });
+  } else {
+    spans.forEach(span => {
+      // Restaura o texto quando a tela é maior que 600px
+      span.childNodes.forEach(child => {
+        if (child.nodeType === Node.TEXT_NODE) {
+          if (span.classList.contains('pop')) {
+            child.textContent = 'Popular'; // Restaura o texto de "Popular"
+          } else if (span.classList.contains('freeshipping')) {
+            child.textContent = 'Entrega'; // Restaura o texto de "Entrega"
+          }
+        }
+      });
+
+      // Restaura o tamanho original do ícone quando a tela for maior que 600px
+      const icon = span.querySelector('i');
+      if (icon) {
+        icon.style.fontSize = ''; // Restaura o tamanho original do ícone
+      }
+
+      // Restaura o padding original da tag <span>
+      span.style.padding = ''; // Restaura o padding original
+    });
+  }
+}
+
+// Chama a função sempre que a janela é redimensionada
+window.addEventListener('resize', adjustDisplay);
+
+// Chama a função também ao carregar a página
+window.addEventListener('load', adjustDisplay);
